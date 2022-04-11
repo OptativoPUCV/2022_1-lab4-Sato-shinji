499fff84b030e1e976cd214f4de3f9a10c262f4e
Sebastian <sebastian.aguilera.a01@mail.pucv.cl>
2022-04-10 21:44:38 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -80,7 +80,16 @@ void eraseMap(HashMap * map,  char * key) {
 }
 
 Pair * searchMap(HashMap * map,  char * key) {   
+    Pair *par;
+    Pair **buckets = map->buckets;
+    long index = hash(key, map->capacity);
 
+    while(buckets[index])
+    {
+        if (!strcmp(buckets[index]->key, key)) return buckets[index];
+        index++;
+        if (index >= map->capacity) index = 0;
+    }
 
     return NULL;
 }
